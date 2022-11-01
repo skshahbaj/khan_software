bcbab6707abcfe4fb53859244a8fb30fd19911cb
shahbaj khan <you@example.com>
2022-11-01 13:15:10 +0530

    mongodb

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,22 +1,22 @@
 import pro from "../models/product.models.js";
 export const proCreate = async (req, res) => { 
     try{
-    // var allImages = [];
-    // req.files.forEach(image => {
-    //     var imageType = '';
-    //     if (image.mimetype == 'image/png') {
-    //         imageType = 'png';
-    //     } else if (image.mimetype == 'image/jpg' || image.mimetype == 'image/jpeg') {
-    //         imageType = 'jpg';
-    //     }
-    //     let imageData = {
-    //         path: image.filename,
-    //         fullpath: "localhost:3002/" + image.path,
-    //         type: imageType,
-    //     }
-    //     allImages.push(imageData)
-    // });
-    // req.body.images = allImages
+    var allImages = [];
+    req.files.forEach(image => {
+        var imageType = '';
+        if (image.mimetype == 'image/png') {
+            imageType = 'png';
+        } else if (image.mimetype == 'image/jpg' || image.mimetype == 'image/jpeg') {
+            imageType = 'jpg';
+        }
+        let imageData = {
+            path: image.filename,
+            fullpath: "localhost:3002/" + image.path,
+            type: imageType,
+        }
+        allImages.push(imageData)
+    });
+    req.body.images = allImages
 
     const existname = await pro.findOne({name:req.body.name})
     if(existname){
