07f5b981456fa43c5f1f1a91ac8fefbdf32b1af2
shahbaj khan <you@example.com>
2022-11-09 00:21:00 +0530

    delet

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,83 +8,89 @@ import users from "../models/user.models.js";
 
 export const signup = async (req, res) => {
    try {
-      const existname = await users.findOne({username:req.body.username})
-      if(existname){
+      const existname = await users.findOne({ username: req.body.username })
+      if (existname) {
          res.send({
-            status:false,
-            msg:"name is already exist",
-            data:{}
+            status: false,
+            msg: "name is already exist",
+            data: {}
          });
-      }else{
+      } else {
          var data1 = (req.body)
-      var passwordhash = await bcrypt.hash(req.body.password, 10);
-      data1.password = passwordhash
-      // var data = ({
-      //    time:Date(),
-      //    userid:"_id"
-      // });
-      // var token = await Jwt.sign(data,"data-token")
-      // data1.token = token
+         var passwordhash = await bcrypt.hash(req.body.password, 10);
+         data1.password = passwordhash
+         var data = ({
+            time: Date(),
+            userid: "_id"
+         });
+         var token = await Jwt.sign(data, "data-token")
+         data1.token = token
 
-      const user = await users.create(req.body);
-      res.send({
-         status:true,
-         msg:"signup succefull ",
-         data:user
-      })
+         const user = await users.create(req.body);
+         res.send({
+            status: true,
+            msg: "signup succefull ",
+            data: user
+         })
       }
-   }catch(err){
+   } catch (err) {
       res.send({
-         status:false,
-         msg:"something wrong",
-         data:err
+         status: false,
+         msg: "something wrong",
+         data: err
       });
    }
-   }
+}
 
 export const login = async (req, res) => {
-   try{
-   var getuser = await users.findOne({username:req.body.username})
-   //  res.send(getuser)
-    const password= await bcrypt.compare(req.body.password,getuser.password)
-    console.log(password)
-    if(password==password){
-      var data = ({
-         time:Date(),
-         userid:"_id"
-      });
-      getuser.token = await Jwt.sign(data,"data-token")
-      // console.log(data)
-      res.send({
-         status:true,
-         msg:"login succefull ",
-         data:getuser
-      }) 
-    }else{
-      res.send({
-         status:false,
-         msg:"data not found ",
-         data:{}
-      })
-     }     
+   try {
+      var getuser = await users.findOne({ username: req.body.username })
+      //   res.send(getuser)
+      if (getuser) {
+         const password = await bcrypt.compare(req.body.password, getuser.password)
+         if (password) {
+             console.log(password)
+            var token = await Jwt.sign({ time: Date(), userid: getuser._id }, "data-token")
 
-   }catch(err){
+             console.log(token)
+            res.send({
+               status: true,
+               msg: "login succefull ",
+               data: getuser
+            })
+         } else {
+            res.send({
+               status: false,
+               msg: "passwor wrong ",
+               data: {}
+            })
+         }
+      } else {
+         res.send({
+            status: false,
+            msg: "username wrong",
+            data: {}
+         })
+      }
+
+
+   } catch (err) {
       res.send({
-         status:false,
-         msg:"data not found ",
-         data:err
+         status: false,
+         msg: "something wrong  ",
+         data: err
       })
    }
-   
+
 }
 // export const allData = async (req, res) => {
-   //  var where = {}
-   //  if (req.query.username){
-   //    where.username = req.query.username
-   //  }
-   //  if (req.query.eamil){
-   //    where.email = req.query.email
-   //  }
+//  var where = {}
+//  if (req.query.username){
+//    where.username = req.query.username
+//  }
+//  if (req.query.eamil){
+//    where.email = req.query.email
+//  }
 //    const data = await users.find()
 //    if (data.length > 0) {
 //       res.send({
@@ -165,7 +171,7 @@ export const deletedata = async (req, res) => {
             msg: "delete succfull",
             data: data
          })
-      }else{
+      } else {
          res.send({
             status: true,
             msg: "delete succfull",
